531352617d732de1703010d4d72e4642304f69d5
Aditya Sethi <adityasethi2004@gmail.com>
2024-09-14 02:16:24 +0530

    checkpoints

---
+++ backend/src/managers/IoManager.ts
@@ -0,0 +1,18 @@
+import { Server } from "socket.io";
+
+export class IoManager
+{
+    private static io;
+    private static instance: IoManager;
+
+    public static getIo(io: Server)
+    {
+        if(!this.instance)
+        {
+            this.instance = new IoManager();
+        }
+        return this.instance;
+    }
+
+
+}
